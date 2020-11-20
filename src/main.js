c81860cdd9fa047a3392da4aba768cfc78f5b841
Benjamin McFarland <benrmcfarland@gmail.com>
2020-11-20 09:33:08 -0800

    add UI code to make api call and return result

--- src/main.js
+++ src/main.js
@@ -2,5 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { Currency } from './js/currency.js';
 
+Currency;
 $;
+
+function getElements(response) {
+  if (response.main) {
+    $("#result").html(`Your currency in EUR is: ${response.conversion_rates.EUR}`);
+  } else {
+    $("#result").html(`There was an error: ${response}`);
+  }
+}
+
+async function apiCall() {
+  const response = await Currency.getCurrency();
+  getElements(response);
+}
+
+$("#submit").submit(function(event) {
+  event.preventDefault();
+  apiCall();
+});
